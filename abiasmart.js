4bd9c618fe7102caa5279af3370a5289921f19d2
unknown <{Stanlee97}>
2025-10-23 20:52:57 +0100

    Updated site

--- abiasmart.js
+++ abiasmart.js
@@ -1,5 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger-menu');
+    const navLinks = document.getElementById('nav-links');
 
-        function checkScreen() {
+    if (hamburger && navLinks) {
+        hamburger.addEventListener('click', function () {
+            navLinks.classList.toggle('active');
+        });
+    } else {
+        console.error("Element(s) with ID 'hamburger-menu' or 'nav-links' not found.");
+    }
+});
+
+const hamburger = document.getElementById('hamburger-menu');
+        const navLinks = document.getElementById('nav-links');
+        
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+
+function checkScreen() {
   if (window.innerWidth < 600) {
     document.body.style.background = "#f0f0f0";
   } else {
@@ -9,3 +28,13 @@
 
 window.addEventListener("resize", checkScreen);
 checkScreen();
+
+// document.getElementById('link-1').addEventListener('click', function() {
+//     navigateToLMS();
+// }
+// );
+
+// function navigateToLMS() {
+//     window.location.href = "https://stanlee97.github.io/Abia-state/";
+// }
+
